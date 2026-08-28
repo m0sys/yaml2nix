2af38d0bdb43e4d5a5bf7ad26edee920227905a5
M0 <21181457+m0sys@users.noreply.github.com>
2026-08-28 10:47:03 -0400

    chore: gen flake.nix using crate2nix init

--- flake.nix
+++ flake.nix
@@ -1,32 +1,75 @@
 {
+  description = "Rust-Nix";
+
   inputs = {
-    cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.12";
-    flake-utils.follows = "cargo2nix/flake-utils";
-    nixpkgs.follows = "cargo2nix/nixpkgs";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    crate2nix.url = "github:nix-community/crate2nix";
+
+    # Development
+
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "eigenvalue.cachix.org-1:ykerQDDa55PGxU25CETy9wF6uVDpadGGXYrFNJA3TUs=";
+    extra-substituters = "https://eigenvalue.cachix.org";
+    allow-import-from-derivation = true;
   };
 
   outputs =
-    inputs:
-    with inputs;
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ cargo2nix.overlays.default ];
-        };
+    inputs @ { self
+    , nixpkgs
+    , flake-parts
+    , rust-overlay
+    , crate2nix
+    , ...
+    }: flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
 
-        rustPkgs = pkgs.rustBuilder.makePackageSet {
-          rustVersion = "1.70.0";
-          packageFun = import ./Cargo.nix;
-        };
+      imports = [
+        ./nix/rust-overlay/flake-module.nix
+        ./nix/devshell/flake-module.nix
+      ];
 
-      in
-      rec {
-        packages = {
-          yaml2nix = (rustPkgs.workspace.yaml2nix { }).bin;
-          default = packages.yaml2nix;
+      perSystem = { system, pkgs, lib, inputs', ... }:
+        let
+          # If you dislike IFD, you can also generate it with `crate2nix generate` 
+          # on each dependency change and import it here with `import ./Cargo.nix`.
+          cargoNix = inputs.crate2nix.tools.${system}.appliedCargoNix {
+            name = "rustnix";
+            src = ./.;
+          };
+        in
+        rec {
+          checks = {
+            rustnix = cargoNix.rootCrate.build.override {
+              runTests = true;
+            };
+          };
+
+          packages = {
+            rustnix = cargoNix.rootCrate.build;
+            default = packages.rustnix;
+
+            inherit (pkgs) rust-toolchain;
+
+            rust-toolchain-versions = pkgs.writeScriptBin "rust-toolchain-versions" ''
+              ${pkgs.rust-toolchain}/bin/cargo --version
+              ${pkgs.rust-toolchain}/bin/rustc --version
+            '';
+          };
         };
-      }
-    );
+    };
 }
