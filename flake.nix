16ebae023bebc5cda08bf2df71e6e010d5b335da
M0 <21181457+m0sys@users.noreply.github.com>
2026-08-29 13:17:57 -0400

    chore: add commit hashes to flake inputs

--- flake.nix
+++ flake.nix
@@ -3,24 +3,25 @@
 
   inputs = {
     flake-parts = {
-      url = "github:hercules-ci/flake-parts";
+      url = "github:hercules-ci/flake-parts/9d0d87172c374f89da73c1cfe6d81ae62feac1f1";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.url = "github:oxalica/rust-overlay/996e9b0b019a4a9eb9e9a5641aefa06d801b5895";
     crate2nix.url = "github:nix-community/crate2nix/0.15.0";
 
     # Development
 
     devshell = {
-      url = "github:numtide/devshell";
+      url = "github:numtide/devshell/255a2b1725a20d060f566e4755dbf571bbbb5f76";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
   nixConfig = {
-    extra-trusted-public-keys = "eigenvalue.cachix.org-1:ykerQDDa55PGxU25CETy9wF6uVDpadGGXYrFNJA3TUs=";
-    extra-substituters = "https://eigenvalue.cachix.org";
-    allow-import-from-derivation = true;
+    # NOTE: enable for IFD and cache optimization
+    ##extra-trusted-public-keys = "eigenvalue.cachix.org-1:ykerQDDa55PGxU25CETy9wF6uVDpadGGXYrFNJA3TUs=";
+    ##extra-substituters = "https://eigenvalue.cachix.org";
+    ##allow-import-from-derivation = true;
   };
 
   outputs =
