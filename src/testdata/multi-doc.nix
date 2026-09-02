14151dd1183e28ba5acaf9571a4ec93f7cbe07ed
M0 <21181457+m0sys@users.noreply.github.com>
2026-09-02 09:54:42 -0400

    fix: make tests pass

--- src/testdata/multi-doc.nix
+++ src/testdata/multi-doc.nix
@@ -1,1 +1,1 @@
-[{ apiVersion = "v1"; kind = "Namespace"; metadata = { name = "foo"; }; } { apiVersion = "v1"; kind = "Namespace"; metadata = { name = "bar"; }; }]
+[ null { apiVersion = "v1"; kind = "Namespace"; metadata = { name = "foo"; }; } { apiVersion = "v1"; kind = "Namespace"; metadata = { name = "bar"; }; } ]
